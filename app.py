3cc0c10c06e91c6ccfe7f9ff725a1963bac3fc7c
Daniel Bortolato <daniel.bortolato@gmail.com>
2022-04-09 22:23:37 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+import datetime
+
+from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(
+        "mongodb+srv://danbortolato:QeVA2QZ9Lfh3Rj1k@cluster.cqlzl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    app.db = client.projetoblog
+
+    @app.route("/", methods=["GET", "POST"])
+    def home():
+        if request.method == "POST":
+            entry_content = request.form.get("content")
+            format_data = datetime.datetime.today().strftime("%Y-%m-%d")
+            app.db.dados.insert_one(
+                {"content": entry_content, "date": format_data})
+
+        data_dado = [
+            (
+                dado["content"],
+                dado["date"],
+                datetime.datetime.strptime(
+                    dado["date"], "%Y-%m-%d").strftime("%b %d")
+            )
+            for dado in app.db.dados.find({})
+        ]
+        return render_template("home.html", dados=data_dado)
+    return app
